06e7d7c8f92ae107cc2f59ed11352ec3fbc3b1d4
KalashM <marina.kalashnyk@gmail.com>
2021-10-07 22:56:03 +0300

    Main class updated to demonstrate polymorphism

--- task2/main/java/com/example/figures/Main.java
+++ task2/main/java/com/example/figures/Main.java
@@ -3,13 +3,6 @@ package com.example.figures;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("----- New Rectangle creation: -------");
-        Rectangle rectangle = new Rectangle(5, 2, 0, 3);
-        rectangle.show();
-        rectangle.move(5, 0);
-        rectangle.zoomPercentage(125);
-        rectangle.show();
-
         System.out.println("----- New Triangle creation: -------");
         Triangle triangle = new Triangle(3,4,5,0,0);
         if (triangle.ifExists()) {
@@ -20,14 +13,16 @@ public class Main {
         } else
             System.out.println("The triangle does not exist");
 
-        System.out.println("----- New Square creation: -------");
-        Rectangle square = new Square(3, 2,2);
-        square.show();
-        square.move(10, 10);
-        square.zoomPercentage(200);
-        square.show();
-
-        Square square1 = new Square(4,1,1);
-        square1.show();
+        System.out.println("----- New Square and Rectangle creation: -------");
+        Rectangle rec[] = new Rectangle[10];
+        for (int i = 0; i <= 4; i++) {
+            rec[i] = new Rectangle(i+1, i+2, i+3, i+4);
+        }
+        for (int i = 5; i <= 9; i++) {
+            rec[i] = new Square(i, i+2, i+3);
+        }
+        for (int i = 0; i <= 9; i++) {
+            rec[i].show();
+        }
     }
 }
