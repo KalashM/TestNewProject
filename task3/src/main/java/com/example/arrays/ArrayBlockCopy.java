d8cba02e498e6b4b6345b6c2871e42e1ac62669a
KalashM <marina.kalashnyk@gmail.com>
2021-12-15 00:31:52 +0200

    arrayBlockCopy methods updated. Logging added in Demo class

--- task3/src/main/java/com/example/arrays/ArrayBlockCopy.java
+++ task3/src/main/java/com/example/arrays/ArrayBlockCopy.java
@@ -2,21 +2,53 @@ package com.example.arrays;
 
 public class ArrayBlockCopy {
 
-    public static void arrayBlockCopy(int[] arrayFrom, int[] arrayTo, int indexFrom, int indexTo) {
+    public static int[] arrayBlockCopy(int[] arrayFrom, int from, int to) {
 
-        if ((indexFrom < arrayFrom.length) && (indexTo < arrayFrom.length)) {
-            for (int i = indexFrom; i <= indexTo; i++) {
+        if ((from < 0) || (from > arrayFrom.length)) {
+            throw new ArrayIndexOutOfBoundsException("Specify the correct from index");
+        } else if (from > to) {
+            throw new IllegalArgumentException("To index should be less then From index");
+        } else {
+            int[] arrayTo = new int[to - from];
+            int j = 0;
+            for (int i = from; i < to; i++) {
                 int value = arrayFrom[i];
-                arrayTo[i] = value;
+                arrayTo[j] = value;
+                j++;
+            }
+            return arrayTo;
+        }
+    }
+
+    public static void arrayBlockCopy(int[] arrayFrom, int[] arrayTo, int from, int to) {
+
+        if ((from < 0) || (from > arrayFrom.length)) {
+            throw new ArrayIndexOutOfBoundsException("Specify the correct from index");
+        } else if ((from > to) || ((arrayTo.length < (to - from)))) {
+            throw new IllegalArgumentException("To index should be less then From index OR arrayTo length is less then index range");
+        } else {
+            int j = 0;
+            if ((from < arrayFrom.length) && (to < arrayFrom.length)) {
+                for (int i = from; i < to; i++) {
+                    int value = arrayFrom[i];
+                    arrayTo[j] = value;
+                    j++;
+                }
             }
         }
     }
 
-    public static void arrayBlockCopyIntoBeginning(int[] arrayFrom, int[] arrayTo, int indexFrom, int indexTo) {
+    public static void arrayBlockCopy(int[] arrayFrom, int[] arrayTo, int from, int to, int inFrom) {
 
-        int j = 0;
-        if ((indexFrom < arrayFrom.length) && (indexTo < arrayFrom.length)) {
-            for (int i = indexFrom; i <= indexTo; i++) {
+        if ((from < 0) || (from > arrayFrom.length))  {
+            throw new ArrayIndexOutOfBoundsException("Specify the correct from index");
+        } else if ((inFrom < 0) || (inFrom > arrayTo.length)) {
+            throw new ArrayIndexOutOfBoundsException("Specify the correct inFrom index");
+        } else if ((from > to) || ((arrayTo.length < (to - from)))) {
+            throw new IllegalArgumentException("To index should be less then From index OR arrayTo length is less then index range");
+        } else {
+            int j = inFrom;
+            for (int i = from; i < to; i++) {
                 int value = arrayFrom[i];
                 arrayTo[j] = value;
                 j++;
--- task3/src/main/java/com/example/arrays/Demo.java
+++ task3/src/main/java/com/example/arrays/Demo.java
@@ -1,29 +1,67 @@
 package com.example.arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Demo {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(Demo.class);
+
     public static void main(String[] args) {
+
         int[] arrayCopyFrom = new int[20];
         int[] arrayCopyTo = new int[20];
         arrayCopyFrom[0] = 0;
         for (int i = 1; i < arrayCopyFrom.length; i++) {
-            arrayCopyFrom[i] = arrayCopyFrom[i-1] + 5;
+            arrayCopyFrom[i] = arrayCopyFrom[i - 1] + 5;
         }
         arrayCopyTo[0] = 3;
         for (int i = 1; i < arrayCopyTo.length; i++) {
-            arrayCopyTo[i] = arrayCopyTo[i-1] + 3;
+            arrayCopyTo[i] = arrayCopyTo[i - 1] + 3;
         }
 
-        ArrayBlockCopy.arrayBlockCopy(arrayCopyFrom, arrayCopyTo, 6, 10);
-
-        ArrayBlockCopy.arrayBlockCopyIntoBeginning(arrayCopyTo, arrayCopyFrom, 18,19);
-
+        LOGGER.debug("ArrayTo initial values:");
         for (int i = 0; i < arrayCopyTo.length; i++) {
-            System.out.println(arrayCopyTo[i]);
+            LOGGER.debug(String.valueOf(arrayCopyTo[i]));
+        }
+        LOGGER.debug("ArrayFrom initial values:");
+        for (int i = 0; i < arrayCopyFrom.length; i++) {
+            LOGGER.debug(String.valueOf(arrayCopyFrom[i]));
+        }
+        LOGGER.info("Copying values range from arrayFrom to arrayTo into the beginning started.");
+        try {
+            ArrayBlockCopy.arrayBlockCopy(arrayCopyFrom, arrayCopyTo, 6, 10);
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            LOGGER.error(e.getMessage());
         }
 
+        LOGGER.info("Copying values range from arrayTo to arrayFrom into the specified position started.");
+        try {
+            ArrayBlockCopy.arrayBlockCopy(arrayCopyTo, arrayCopyFrom, 18,19, 5);
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        LOGGER.info("Copying values range from arrayFrom into a new array started.");
+        int[] arrayNew;
+        try {
+            arrayNew = ArrayBlockCopy.arrayBlockCopy(arrayCopyFrom, 5,10);
+            LOGGER.info("New array values:");
+            for (int i = 0; i < arrayNew.length; i++) {
+                LOGGER.info(String.valueOf(arrayNew[i]));
+            }
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        LOGGER.info("ArrayFrom values after modifications:");
         for (int i = 0; i < arrayCopyFrom.length; i++) {
-            System.out.println(arrayCopyFrom[i]);
+            LOGGER.info(String.valueOf(arrayCopyFrom[i]));
+        }
+
+        LOGGER.info("ArrayTo values after modifications:");
+        for (int i = 0; i < arrayCopyTo.length; i++) {
+            LOGGER.info(String.valueOf(arrayCopyTo[i]));
         }
     }
 }
