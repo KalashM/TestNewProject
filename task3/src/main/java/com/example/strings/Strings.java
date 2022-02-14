224a376e8334fa0a9b662624213345db2e676375
KalashM <marina.kalashnyk@gmail.com>
2022-02-15 00:08:43 +0200

    findCommonSymbols initial version added

--- task3/src/main/java/com/example/strings/Strings.java
+++ task3/src/main/java/com/example/strings/Strings.java
@@ -1,5 +1,9 @@
 package com.example.strings;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Strings {
 
     public static String stringReverseUsingBuildInMethod(String s) {
@@ -26,6 +30,43 @@ public class Strings {
             String newString  = String.valueOf(temp);
             return newString;
         }
+    }
+
+    public static char[] findCommonSymbols(String[] s) {
+        int len;
+        int min = s[0].length();
+        int indexOfShortestElement = 0;
+        for (int i = 1; i < s.length; i++) {
+            if (s[i].length() < min) {
+                min = s[i].length();
+                indexOfShortestElement = i;
+            }
+        }
+
+        String shortestString = s[indexOfShortestElement];
+
+        char[] chars = shortestString.toCharArray();
+        Set<Character> charSet = new LinkedHashSet<Character>();
+        for (char c : chars) {
+            charSet.add(c);
+        }
+
+        StringBuilder resultString = new StringBuilder();
+
+        for (Character character : charSet) {
+            int count = 0;
+            for (String s1: s) {
+                if (s1.contains(Character.toString(character))) {
+                    count++;
+                }
+            }
+            if (count == s.length) {
+                resultString.append(character);
+            }
+        }
+
+        return resultString.toString().toCharArray();
+
     }
 
 }
