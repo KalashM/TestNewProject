7be4a2f467eb081004dd6d8d3e04fc7e88b9fcdd
KalashM <marina.kalashnyk@gmail.com>
2022-07-01 00:48:12 +0300

    Unit tests added for findFilesByPattern method

--- task4/src/test/java/com/example/files/FilesTest.java
+++ task4/src/test/java/com/example/files/FilesTest.java
@@ -1,12 +1,35 @@
 package com.example.files;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FilesTest {
 
-    @Test
-    void findFilesByPatternTest() {
+    @ParameterizedTest
+    @MethodSource("filesByPattern")
+    void findFilesByPatternTest(String[] actual, String[] expected) {
+        File[] actualFiles = Files.findFilesByPattern(actual[0], actual[1]);
+        List<String> actualFileNames = new ArrayList<String>();
+        for (int i = 0; i < actualFiles.length; i++) {
+            actualFileNames.add(actualFiles[i].getName());
+        }
+        assertArrayEquals(expected, actualFileNames.toArray());
+    }
+
+    public static Stream<Arguments> filesByPattern() {
+        return Stream.of(
+                Arguments.of(new String[]{"D:\\", "(.*)fo(.*)"}, new String[]{"foto.7z"}),
+                Arguments.of(new String[]{"D:\\Java\\testFolder", "(.*).xml"}, new String[]{"test2.xml", "test3.xml"})
+        );
     }
 }
