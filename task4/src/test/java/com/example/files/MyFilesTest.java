3ac164889a7c577406ae7a931b89ead425a18c8b
KalashM <marina.kalashnyk@gmail.com>
2023-01-19 18:22:28 +0200

    Tests updated to avoid hardcoded absolute pathes

--- task4/src/test/java/com/example/files/MyFilesTest.java
+++ task4/src/test/java/com/example/files/MyFilesTest.java
@@ -5,17 +5,26 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MyFilesTest {
 
     @ParameterizedTest
     @MethodSource("filesByPattern")
-    void findFilesByPatternTest(String[] actual, String[] expected) {
-        File[] actualFiles = MyFiles.findFilesByPattern(actual[0], actual[1]);
+    void findFilesByPatternTest(String actualFile, String pattern, String[] expected) throws IOException {
+        if (actualFile.equals("src\\test\\resources")) {
+            createFiles();
+        }
+        Path dir = Paths.get(actualFile).toAbsolutePath();
+        File[] actualFiles = MyFiles.findFilesByPattern(dir, pattern);
         List<String> actualFileNames = new ArrayList<String>();
         for (int i = 0; i < actualFiles.length; i++) {
             actualFileNames.add(actualFiles[i].getName());
@@ -25,8 +34,21 @@ class MyFilesTest {
 
     public static Stream<Arguments> filesByPattern() {
         return Stream.of(
-                Arguments.of(new String[]{"D:\\", "(.*)fo(.*)"}, new String[]{"foto.7z"}),
-                Arguments.of(new String[]{"D:\\Java\\testFolder", "(.*).xml"}, new String[]{"test2.xml", "test3.xml"})
+                Arguments.of("src\\test\\java\\com\\example\\files", "(.*)My(.*)", new String[]{"MyFilesTest.java"}),
+                Arguments.of("src\\test\\resources", "(.*).txt", new String[]{"test1.txt", "test2.txt", "test3.txt"})
         );
+    }
+
+    public static void createFiles() throws IOException {
+
+        String[] fileNames = new String[]{"test1.txt", "test2.txt", "test3.txt"};
+
+        for (String fileName: fileNames) {
+            Path filePath = Paths.get("src\\test\\resources" + "\\" + fileName).toAbsolutePath();
+            if (!Files.exists(filePath)) {
+                System.out.println(filePath);
+                Files.createFile(filePath);
+            }
+        }
     }
 }
