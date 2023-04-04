b1eadafa001fff1486f2d1b2572d5adb879bd1bf
KalashM <marina.kalashnyk@gmail.com>
2023-04-04 15:27:24 +0300

    comments removed

--- task5/src/main/java/com/example/diningphilosophers/Demo.java
+++ task5/src/main/java/com/example/diningphilosophers/Demo.java
@@ -1,17 +1,6 @@
 package com.example.diningphilosophers;
 
 public class Demo {
-       /*Philosopher philosopher1 = new Philosopher(1);
-        Philosopher philosopher2 = new Philosopher(2);
-        Philosopher philosopher3 = new Philosopher(3);
-        Philosopher philosopher4 = new Philosopher(4);
-        Philosopher philosopher5 = new Philosopher(5);
-
-        new Thread(philosopher1).start();
-        new Thread(philosopher2).start();
-        new Thread(philosopher3).start();
-        new Thread(philosopher4).start();
-        new Thread(philosopher5).start();*/
 
     public static Philosopher[] createPhilosophers(int n) {
 
