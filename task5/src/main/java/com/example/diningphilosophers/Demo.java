647a80275f62375b946bfb16408e7423457020eb
KalashM <marina.kalashnyk@gmail.com>
2023-04-24 12:56:53 +0300

    Feast class added

--- task5/src/main/java/com/example/diningphilosophers/Demo.java
+++ task5/src/main/java/com/example/diningphilosophers/Demo.java
@@ -2,32 +2,9 @@ package com.example.diningphilosophers;
 
 public class Demo {
 
-    public static Philosopher[] createPhilosophers(int n) {
-
-        Fork[] forks = new Fork[n];
-
-        for (int i = 0; i < n; i++) {
-            forks[i] = new Fork(i);
-        }
-
-        Philosopher[] philosophers = new Philosopher[n];
-
-        for (int i = 0; i < n; i++) {
-
-            Fork leftFork = forks[i];
-            Fork rightFork = forks[(i + 1) % n];
-
-            philosophers[i] = new Philosopher(i, leftFork, rightFork);
-
-        }
-
-        return philosophers;
-
-    }
-
     public static void main(String[] args) {
 
-        Philosopher[] philosophers = createPhilosophers(5);
+        Philosopher[] philosophers = Feast.createPhilosophers(5);
 
         for (Philosopher philosopher : philosophers) {
             philosopher.start();
