a84a5cb57c5c3196eec3ddbf85c3162dee6d6b18
KalashM <marina.kalashnyk@gmail.com>
2023-09-26 18:11:10 +0300

    Demo updated to use localized messages

--- task6/src/main/java/com/example/beansvalidationdemo/Demo.java
+++ task6/src/main/java/com/example/beansvalidationdemo/Demo.java
@@ -4,16 +4,28 @@ import com.example.beansvalidation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Demo {
 
     private static Logger LOGGER = LoggerFactory.getLogger(Demo.class);
 
     public static void main(String[] args) {
+
+        Locale[] supportedLocales = {
+                Locale.ENGLISH,
+                Locale.GERMAN,
+                new Locale("ua", "UA")
+        };
+
         Employee emp = new Employee();
-        //emp.setId(10);
         emp.setEmail("marina.kalashnyk#gmail.com");
+
+        ResourceBundle labels = ResourceBundle.getBundle("EmployeeBundle", supportedLocales[2]);
+
         for (String message : Validator.validate(emp)) {
-            LOGGER.info(message);
+            LOGGER.info(labels.getString(message));
         }
     }
 }
