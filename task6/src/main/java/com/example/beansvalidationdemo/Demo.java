3a362ff066aca2076a29343a00af204abf93a4ad
KalashM <marina.kalashnyk@gmail.com>
2023-09-27 15:53:08 +0300

    Properties updated, Demo updated to use arguments for main method

--- task6/src/main/java/com/example/beansvalidationdemo/Demo.java
+++ task6/src/main/java/com/example/beansvalidationdemo/Demo.java
@@ -4,6 +4,7 @@ import com.example.beansvalidation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -12,20 +13,23 @@ public class Demo {
     private static Logger LOGGER = LoggerFactory.getLogger(Demo.class);
 
     public static void main(String[] args) {
-
-        Locale[] supportedLocales = {
-                Locale.ENGLISH,
-                Locale.GERMAN,
-                new Locale("ua", "UA")
-        };
-
+        Locale locale = Locale.ROOT;
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("en")) {
+                locale = Locale.ENGLISH;
+            } else if (args[0].equalsIgnoreCase("de")) {
+                locale = Locale.GERMAN;
+            } else if (args[0].equalsIgnoreCase("ukr")) {
+                locale = new Locale.Builder().setLanguage("ukr").setScript("Cyrl").build();
+            }
+        }
         Employee emp = new Employee();
         emp.setEmail("marina.kalashnyk#gmail.com");
 
-        ResourceBundle labels = ResourceBundle.getBundle("EmployeeBundle", supportedLocales[2]);
+        ResourceBundle labels = ResourceBundle.getBundle("EmployeeBundle", locale);
 
         for (String message : Validator.validate(emp)) {
-            LOGGER.info(labels.getString(message));
+           LOGGER.info(new String(labels.getString(message).getBytes(), StandardCharsets.UTF_8));
         }
     }
 }
