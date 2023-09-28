25b2aa49b1477acc8e90094c2ce35f061888fb94
KalashM <marina.kalashnyk@gmail.com>
2023-09-28 14:59:44 +0300

    Demo main updated to use locale from locale-config.preperties file

--- task6/src/main/java/com/example/beansvalidationdemo/Demo.java
+++ task6/src/main/java/com/example/beansvalidationdemo/Demo.java
@@ -4,23 +4,38 @@ import com.example.beansvalidation.Validator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class Demo {
 
     private static Logger LOGGER = LoggerFactory.getLogger(Demo.class);
+    private static String localeConfigPath = Thread.currentThread().getContextClassLoader().getResource("locale-config.properties").getPath();
 
     public static void main(String[] args) {
+
+        Properties localeProps = new Properties();
+        try {
+            localeProps.load(new FileInputStream(localeConfigPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String loc = localeProps.getProperty("locale");
+
         Locale locale = Locale.ROOT;
-        if (args.length > 0) {
-            if (args[0].equalsIgnoreCase("de")) {
+        if (!loc.isBlank() && !loc.isEmpty()) {
+            if (loc.equalsIgnoreCase("de")) {
                 locale = Locale.GERMAN;
-            } else if (args[0].equalsIgnoreCase("ukr")) {
+            } else if (loc.equalsIgnoreCase("ukr")) {
                 locale = new Locale.Builder().setLanguage("ukr").setScript("Cyrl").build();
             }
         }
+
         Employee emp = new Employee();
         emp.setEmail("marina.kalashnyk#gmail.com");
 
