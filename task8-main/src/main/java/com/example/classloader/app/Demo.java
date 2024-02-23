7803834a8db84c93586a5a2b3acff189ee467b30
KalashM <marina.kalashnyk@gmail.com>
2024-02-23 21:00:41 +0200

    Inintial version of performCalculation method added to load classes from a directory

--- task8-main/src/main/java/com/example/classloader/app/Demo.java
+++ task8-main/src/main/java/com/example/classloader/app/Demo.java
@@ -1,22 +1,42 @@
 package com.example.classloader.app;
 
-import com.example.classloader.Calculable;
-import com.example.classloader.implement.CalculateAdd;
-import com.example.classloader.implement.CalculateMultiply;
-import com.example.classloader.implement.CalculateSquareRoot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-
+@SuppressWarnings("unchecked")
 public class Demo {
     private static final Logger LOGGER = LoggerFactory.getLogger(Demo.class);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+
+        List<File> jars = Arrays.asList(new File("task8-main/build/libs/moduls").listFiles());
+        URL[] urls = new URL[jars.size()];
+        for (int i = 0; i < jars.size(); i++) {
+            try {
+                urls[i] = jars.get(i).toURI().toURL();
+                System.out.println(urls[i]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        URLClassLoader childClassLoader = new URLClassLoader(urls, ClassLoader.getSystemClassLoader());
+        Class.forName("com.example.classloader.Calculable", true, childClassLoader);
+        Class.forName("com.example.classloader.implement.CalculateAdd", true, childClassLoader);
+        Class.forName("com.example.classloader.implement.CalculateMultiply", true, childClassLoader);
+        Class.forName("com.example.classloader.implement.CalculateSquareRoot", true, childClassLoader);
+        Thread.currentThread().setContextClassLoader(childClassLoader);
 
         //TO DO: Get the list of available operations programmatically.
         LOGGER.info("Enter the operation name for calculation (Add/Multiply/Square Root): ");
@@ -28,7 +48,7 @@ public class Demo {
 
         Scanner in = new Scanner(System.in);
 
-        ArrayList<Double> paramList = new ArrayList<Double>();
+        ArrayList<Double> paramList = new ArrayList<>();
         while (in.hasNextDouble()) {
             double param = in.nextDouble();
             paramList.add(param);
@@ -36,24 +56,31 @@ public class Demo {
 
         LOGGER.info(paramList.toString());
 
-        Calculable calculate;
         double result = 0;
         switch (operation) {
             case "Add":
-                calculate = new CalculateAdd(paramList);
-                result = calculate.getResult();
+                result = performCalculation(childClassLoader, "Add", paramList);
                 break;
-            case "Multiply":
+            /*case "Multiply":
                 calculate = new CalculateMultiply(paramList);
                 result = calculate.getResult();
                 break;
             case "Square Root":
                 calculate = new CalculateSquareRoot(paramList);
                 result = calculate.getResult();
-                break;
+                break;*/
             default:
                 LOGGER.info("The operation name entered is not correct!");
         }
         LOGGER.info(String.valueOf(result));
+    }
+
+    private static double performCalculation(URLClassLoader childClassLoader, String operationName, ArrayList<Double> paramList) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        Class[] parameterType = new Class[]{ArrayList.class};
+        Class myClass = childClassLoader.loadClass("com.example.classloader.implement.CalculateAdd");
+        Constructor constructor = myClass.getConstructor(parameterType);
+        Object calculate = constructor.newInstance(paramList);
+        Method getResultmethod = myClass.getMethod("getResult");
+        return (double) getResultmethod.invoke(calculate);
     }
 }
